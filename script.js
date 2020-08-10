f531cff346ca0c09eb0d09f19010eb9160b7cd1b
lollipas <draakonlohe123@gmail.com>
2020-08-10 14:24:31 +0300

    Updated landing page

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-const professionText = document.querySelector("#profession");
+const professionStr = document.querySelector("#profession");
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelector('.nav-link');
 
-let professions = ["Developer", "Student", "UI/UX Designer", "Programmer"];
+let professions = ["Student", "Web developer", "UI/UX Designer", "Programmer"];
 
-function professionChanger(){
-    for (profession of professions){
-        professionText.innerHTML = profession;
+let pChanger = function(arr){
+    let current = 0
+    let fn = function(){
+        if(current == professions.length) {
+        setTimeout(mainLoop, 1000);
+        professionStr.classList.add('pre-animation');
+        return;
     }
+    arr[current++]
+    professionStr.innerHTML = professions[current-1];
+    
+    setTimeout(fn, 1000);
+    }
+setTimeout(fn, 1000);
 }
 
-setInterval(professionChanger, 1000)
+let i = 0;
+let mainLoop = function(){
+    pChanger(professions[i])
+    professionStr.classList.remove('pre-animation');
+}
+mainLoop()
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('open');
